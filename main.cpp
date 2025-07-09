b928a549349c0b78b078fd836bda07512353dd67
Ricky Lu <rickylu1@outlook.com>
2025-07-09 17:33:13 -0500

    practice

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double value1;
+    double value2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> value1;
+    cout << "Enter second number: ";
+    cin >> value2;
+
+    
+}
