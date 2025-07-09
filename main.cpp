951a24ab89594f7a7374113bdec4977ae3482d44
Ricky Lu <rickylu1@outlook.com>
2025-07-09 18:03:16 -0500

    Added all 4 functionalities

--- main.cpp
+++ main.cpp
@@ -11,5 +11,25 @@ int main(){
     cout << "Enter second number: ";
     cin >> value2;
 
-    
+    cout << "Enter operation you'd like to perform (+, -, *, /):";
+    cin >> operation;
+
+    switch(operation){
+        case '+':
+            cout << "Result: " << value1 + value2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << value1 - value2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << value1 * value2 << endl;
+            break;
+        case '/':
+            if (value2 != 0) {
+                cout << "Result: " << value1 / value2 << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+    }
 }
